2de15a0355c34bcf24840f878f8a4a6d8ce09ee5
Miroslav Galabov <wolfnature13@gmail.com>
2016-02-07 14:55:34 +0200

    Minor changes
    Minor changes.

---
+++ Homeworks/11.Unit-Testing-Homework/11. Unit-Testing-Homework/CustomLinkedList/CustomLinkedListTestCase/ConcreteListTestCase.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using CustomLinkedList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomLinkedListTestCase
+{
+    [TestClass]
+    public class ConcreteListTestCase : DynamicListTestCase
+    {
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            ClassInit(context);
+        }
+
+        protected override DynamicList<string> GetList()
+        {
+            return new DynamicList<string>();
+        }
+    }
+}
---
+++ Homeworks/11.Unit-Testing-Homework/11. Unit-Testing-Homework/CustomLinkedList/CustomLinkedListTestCase/DynamicListTestCase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomLinkedList;
+
+namespace CustomLinkedListTestCase
+{
+    [TestClass]
+    public abstract class DynamicListTestCase
+    {
+        private readonly string ValueA = "A";
+        private readonly string ValueB = "B";
+        private readonly string ValueC = "C";
+        private readonly string ValueD = "D";
+        private DynamicList<string> list;
+
+        protected abstract DynamicList<string> GetList();
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            this.list = GetList();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this.list = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestInsertIntoNegativeIndexThrowsException()
+        {
+            
+        }
+    }
+}
